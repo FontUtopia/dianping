5e4429e8c8116c4daff532b4dd6fdb1de8be0dfc
niuchaoying <niuchaoying@xuetangx.com>
2017-11-24 19:03:32 +0800

    本地数据存储

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import LocalStore from '../util/localStore'
+import {CITYNAME} from '../config/localStoreKey'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userInfoActionsFormOtherFile from '../actions/userinfo'
 
 class App extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+          initDone : false
+        }
     }
     render() {
         return (
             <div>
-                {this.props.children}
+                {
+                  this.state.initDone
+                  ?this.props.children
+                  : <div>加载中...</div>
+                }
             </div>
         )
+    }
+    componentDidMount(){
+      // 从localstorerage里面获取城市
+      let cityName = LocalStore.getItem(CITYNAME)
+      if(cityName == null){
+        cityName = '北京'
+      }
+      console.log(cityName)
+
+      // 将城市信息存储到 Redux 中
+      this.setState({
+        initDone:true
+      })
     }
 }
 
